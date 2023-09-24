5b2fffb9e8e49f7144bb37395f9803dbde23bb1d
zxeveroxz <s.zegarra@hotmail.com>
2023-09-23 21:37:32 -0500

    primer commit

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -220,7 +220,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?> OK</h1>
+        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?> OK openssl_free_key</h1>
 
         <h2>The small framework with powerful features</h2>
 
