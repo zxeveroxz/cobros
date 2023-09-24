51b16cec4f47e066e50441fb481808b22754e52e
Sergio Zegarra <s.zegarra@hotmail.com>
2023-09-24 04:19:29 -0500

    se agrego la configuracion de la base de datos de sql srv

--- public/demo.php
+++ public/demo.php
@@ -3,12 +3,12 @@ $servidor = "25.15.243.250";
 $base = "DBFREST";
 $user="sa";
 $pass="masterkey";
-$info = array('Database'=>$base,'UID'=>$user,'PWD'=>$pass,"TrustServerCertificate"=>false);
+$info = array('Database'=>$base,'UID'=>$user,'PWD'=>$pass,"TrustServerCertificate"=>true);
 $conex= sqlsrv_connect($servidor,$info);
 
 if($conex===false){
 
-    $stmt = sqlsrv_query($conex, "select TOP 10 * from CLIENTES");
+    $stmt = sqlsrv_query($conex, "select TOP 50 * from CLIENTES");
     print_r(sqlsrv_fetch_array($stmt));
 }else{
     die(print_r(sqlsrv_errors(),true));
